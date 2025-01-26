93617264377a9b70e14b3a4922ffc16ca60f22bc
armangracia <graciaarmansteven@gmail.com>
2025-01-27 00:05:40 +0800

    reinstalled packages, drawer navigator testing

--- OptiCool-Mobile/App.js
+++ OptiCool-Mobile/App.js
@@ -3,24 +3,30 @@ import "react-native-safe-area-context";
 
 import { Provider } from "react-redux";
 import { PersistGate } from 'redux-persist/integration/react';
+// import DrawerNavigator from './navigators/DrawerNavigator';
+import 'react-native-gesture-handler';
+
 import { persistor, store } from "./states/store";
 
 import Main from './Main';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function App() {
 
   return (
 
-    <Provider store={store}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
 
-      <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
 
-        <Main />
+        <PersistGate loading={null} persistor={persistor}>
 
-      </PersistGate>
+          <Main />
 
-    </Provider>
+        </PersistGate>
 
+      </Provider>
+
+    </GestureHandlerRootView>
   );
-
 }
---
+++ OptiCool-Mobile/navigators/SimpleDrawerNavigator.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
+import { View, Text } from 'react-native';
+
+const Drawer = createDrawerNavigator();
+
+const Screen1 = () => (
+  <View>
+    <Text>Screen 1</Text>
+  </View>
+);
+
+const Screen2 = () => (
+  <View>
+    <Text>Screen 2</Text>
+  </View>
+);
+
+const SimpleDrawerNavigator = () => {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Screen1"
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: '#fff',
+          width: 240,
+        },
+        headerShown: false,
+        drawerActiveTintColor: '#96a8fa',
+        drawerInactiveTintColor: '#000',
+        drawerLabelStyle: {
+          fontSize: 16,
+        },
+      }}
+    >
+      <Drawer.Screen
+        name="Screen1"
+        component={Screen1}
+        options={{
+          drawerLabel: 'Screen 1',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Screen2"
+        component={Screen2}
+        options={{
+          drawerLabel: 'Screen 2',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+export default SimpleDrawerNavigator;
