452fe19bfd8008200bd368522158c5e803a0e817
armangracia <graciaarmansteven@gmail.com>
2025-01-29 09:45:10 +0800

    drawer update

--- OptiCool-Mobile/navigators/DrawerNavigation.js
+++ OptiCool-Mobile/navigators/DrawerNavigation.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { useSelector } from 'react-redux';
+import { StatusBar } from 'react-native';
 import BottomTabs from './BottomTabs';
+import ProfileNavigation from './ProfileNavigation';
+import AdminDashboard from '../screens/AdminScreens/AdminDashboard';
+import WelcomePage from '../screens/UserScreens/WelcomePage';
+import AuthNavigation from './AuthNavigation';
 
 const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigation() {
+  const { isLogin, user } = useSelector((state) => state.auth);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Check if user is initialized
+  useEffect(() => {
+    if (user !== null) {
+      setIsLoading(false); // Set loading to false once user data is available
+    }
+  }, [user]);
+
+  if (isLoading) {
+    // Show loading state (e.g., spinner) while the authentication state is being checked
+    return null; // Or you can replace this with a loading spinner or placeholder
+  }
+
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        {/* Hide the label for this drawer screen */}
-        <Drawer.Screen
-          name="Tabs"
-          component={BottomTabs}
-          options={{
-            headerShown: false, // Hide the header
+      <StatusBar translucent backgroundColor="transparent" barStyle="dark-content" />
+      {isLogin ? (
+        <Drawer.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            headerShown: false,
           }}
-        />
-      </Drawer.Navigator>
+        >
+          <Drawer.Screen name="Home" component={BottomTabs} />
+          <Drawer.Screen name="Profile" component={ProfileNavigation} />
+          <Drawer.Screen name="AdminDashboard" component={AdminDashboard} />
+        </Drawer.Navigator>
+      ) : (
+        // Render WelcomePage if not logged in
+        <AuthNavigation />
+      )}
     </NavigationContainer>
   );
 }
