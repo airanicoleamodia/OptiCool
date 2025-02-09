dbfcf1bf0259b9517d4e33eda821f78dfd422f64
armangracia <graciaarmansteven@gmail.com>
2025-02-09 11:53:03 +0800

    last push for pre-def

--- OptiCool-Mobile/screens/AdminScreens/HelpDetails.js
+++ OptiCool-Mobile/screens/AdminScreens/HelpDetails.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 const HelpDetails = ({ route }) => {
   const { post } = route.params;
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.title}>{post.title}</Text>
       <Text style={styles.content}>{post.content}</Text>
       <Text style={styles.date}>Last Updated: {new Date(post.updatedAt).toLocaleString()}</Text>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flexGrow: 1,
     padding: 20,
     backgroundColor: "#fff",
   },
